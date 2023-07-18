8410eab743342b8e899a035d3155e6b8c140d046
Pitanic <pitanic007@gmail.com>
2023-07-17 17:52:27 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,20 @@
 #define DOG_H
 
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
+/**	
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
 		char *name;
 		float age;
 		char *owner;
-	};
+};
 
 
 	/**
@@ -24,7 +24,7 @@
 	typedef struct dog dog_t;
 
 
-	void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 	void print_dog(struct dog *d);
 	dog_t *new_dog(char *name, float age, char *owner);
 	void free_dog(dog_t *d);
