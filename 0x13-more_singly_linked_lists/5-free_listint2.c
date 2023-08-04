33c659e45eb2c7e23602d71158f5c0922b309569
Pitanic <pitanic007@gmail.com>
2023-08-04 10:24:33 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - this frees a listint_t list
+ * @head: the listint_t list which will be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
