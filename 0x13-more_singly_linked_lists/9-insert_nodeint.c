9a5799cb3c23d140275850244ea7ca6a9aca7c91
Pitanic <pitanic007@gmail.com>
2023-08-17 02:46:27 -0700

    new node at a position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@
  * @head: points to the first node in the list
  * @idx: index of the list where the new node should be added
  * @n: data to be inserted in the new node
- * Returns: pointer to the address of the new node, or NULL if it failed
+ * Return: pointer to the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
